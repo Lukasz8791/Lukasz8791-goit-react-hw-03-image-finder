1359c920439a9466e41d1041a5ea6ae9b38f4b3a
Lukasz8791 <doktor8on@gmail.com>
2024-01-29 22:39:14 +0100

    updatena7

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,13 +1,10 @@
 import PropTypes from 'prop-types';
-
 const Button = ({ onLoadMore }) => (
   <button type="button" className={styles.button} onClick={onLoadMore}>
     Load more
   </button>
 );
-
 Button.propTypes = {
   onLoadMore: PropTypes.func.isRequired,
 };
-
 export default Button;
--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import style from './Loader.module.css';
-
 const Loader = () => (
   <div className="loaderContainer">
     <div className="loader"></div>
   </div>
 );
-
 export default Loader;
